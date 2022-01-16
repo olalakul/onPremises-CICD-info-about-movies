b4664677e20124ccb1e2f8800f3bad9a6bd8de6f
olalakul <olalakul@gmail.com>
2022-01-16 15:26:14 +0100

    testing session with IMDB

--- tests/test_home.py
+++ tests/test_home.py
@@ -9,3 +9,9 @@ def test_home(client):
     resp = client.get('/')
     assert resp.status_code == 200
     assert resp.mimetype == 'text/html'
+
+def test_home_bad_http_method(client):
+    resp = client.post('/')
+    assert resp.status_code == 405  # 405 = Method Not Allowed 
+
+    
--- tests/test_inquire_imdb.py
+++ tests/test_inquire_imdb.py
@@ -1,4 +1,5 @@
 import pytest
+import requests
 
 import app_info_about_movies.inquire_imdb as iimdb
 
@@ -8,3 +9,16 @@ def test_match_distributors_production():
     distributors_DE = ['Walt Disney Studios Home Entertainment (2020) (all media) (Ultra HD Blu-ray)']
     prodis = iimdb.match_distributors_production(production_companies, distributors_DE, verbose=False)
     assert prodis[0] == 'Walt Disney'
+
+
+@pytest.fixture(scope='session')
+def sess():
+    return requests.Session()
+
+def test_get_ID_frim_IMDB(sess):
+    imdbID, single_found_df = iimdb.get_ID_from_IMDB(sess, 
+                                                        title="Lost", 
+                                                        year=2004, 
+                                                        type_="serie", 
+                                                        verbose=0)
+    assert imdbID == 'tt0411008'
